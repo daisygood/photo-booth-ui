8cb25b0bfca2b28ffbe57d66c9fb95fd4213b71e
goo7387 <daisygood@northwesternmutual.com>
2018-08-21 19:06:02 -0500

    Add prod api link

--- src/config/api.js
+++ src/config/api.js
@@ -3,6 +3,6 @@ let API_URL;
 if (process.env.NODE_ENV === 'development') {
   API_URL = 'http://localhost:8080/api';
 } else {
-  API_URL = '';
+  API_URL = 'http://ec2-34-209-46-18.us-west-2.compute.amazonaws.com/api';
 }
 export default API_URL;
