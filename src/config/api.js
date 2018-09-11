27c7f98e168da670a1ef6fed1b92ead0a30c0dbd
goo7387 <daisygood@northwesternmutual.com>
2018-09-10 20:02:32 -0500

    change api

--- src/config/api.js
+++ src/config/api.js
@@ -3,6 +3,6 @@ let API_URL;
 if (process.env.NODE_ENV === 'development') {
   API_URL = 'http://localhost:8080/api';
 } else {
-  API_URL = 'https://api-env-2.sg4q2tibb2.us-west-2.elasticbeanstalk.com/api';
+  API_URL = 'https://api.thepbcam.com/api';
 }
 export default API_URL;
